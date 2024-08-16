f027b52d8861e1f6691948beed4edac3c95be6bc
Steffen Wiegand <steffen.wiegand82@gmail.com>
2024-08-16 22:02:13 +1000

    Added new parameter orderby to ListRaces. Added unit test method for orderBy. Racing readMe updated.

--- racing/db/races_test.go
+++ racing/db/races_test.go
@@ -13,6 +13,7 @@ import (
 
 // testing the races.go/List function which has optional filters
 func TestRacesList(t *testing.T) {
+
 	// setup racing test data
 	testRaces := []struct {
 		id                  int64
@@ -54,9 +55,65 @@ func TestRacesList(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			races, err := testRacesRepo.List(tc.filter)
+			races, err := testRacesRepo.List(tc.filter, "")
 			require.NoError(t, err)
+
 			assert.Len(t, races, tc.expectedLength, "Unexpected number of races.")
+		})
+	}
+}
+
+// testing the races.go/List function which has optional filters
+func TestRacesListOrderBy(t *testing.T) {
+
+	// setup racing test data
+	testRaces := []struct {
+		id                  int64
+		meetingId           int64
+		name                string
+		number              int64
+		visible             bool
+		advertisedStartTime *timestamppb.Timestamp
+	}{
+		{id: 1, meetingId: 5, name: "Horse Race", number: 91, visible: true, advertisedStartTime: timestamppb.New(time.Date(2009, 11, 21, 0, 0, 0, 0, time.UTC))},
+		{id: 2, meetingId: 1, name: "Dog Race", number: 92, visible: true, advertisedStartTime: timestamppb.New(time.Date(2009, 11, 22, 0, 0, 0, 0, time.UTC))},
+		{id: 3, meetingId: 3, name: "Rabbit Race", number: 93, visible: true, advertisedStartTime: timestamppb.New(time.Date(2009, 11, 23, 0, 0, 0, 0, time.UTC))},
+		{id: 4, meetingId: 4, name: "Rabbit Race", number: 94, visible: false, advertisedStartTime: timestamppb.New(time.Date(2009, 11, 24, 0, 0, 0, 0, time.UTC))},
+		{id: 5, meetingId: 2, name: "Snake Race", number: 95, visible: false, advertisedStartTime: timestamppb.New(time.Date(2009, 11, 25, 0, 0, 0, 0, time.UTC))},
+	}
+
+	// setup test db
+	db := setupTestDB(t)
+	defer db.Close()
+
+	// create table
+	createTestTable(t, db)
+
+	// insert test records
+	insertTestRaces(t, db, testRaces)
+
+	testRacesRepo := NewRacesRepo(db)
+
+	// test cases
+	testCases := []struct {
+		name            string
+		orderBy         string
+		expectedIdOrder []int
+	}{
+		{name: "no orderBy", orderBy: "", expectedIdOrder: []int{1, 2, 3, 4, 5}},
+		{name: "advertised_start_time DESC", orderBy: "advertised_start_time DESC", expectedIdOrder: []int{5, 4, 3, 2, 1}},
+		{name: "meeting_id", orderBy: "meeting_id", expectedIdOrder: []int{2, 5, 3, 4, 1}},
+		{name: "name, advertised_start_time DESC", orderBy: "name, advertised_start_time DESC", expectedIdOrder: []int{2, 1, 4, 3, 5}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			races, err := testRacesRepo.List(&racing.ListRacesRequestFilter{}, tc.orderBy)
+			require.NoError(t, err)
+
+			for i, race := range races {
+				assert.Equal(t, int(tc.expectedIdOrder[i]), int(race.Id), "Unexpected race id for test case %s. Expected race id is %d. Returned was %d.", tc.name, tc.expectedIdOrder[i], race.Id)
+			}
 		})
 	}
 }
